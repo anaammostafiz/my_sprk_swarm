08ab5022a4ae85f84c5ddff9133777eaa26e0f73
anaammostafiz <anaam.mostafiz@gmail.com>
2023-10-22 18:07:43 -0700

    form circle with successful robot avoidance

--- scripts/form_circle.py
+++ scripts/form_circle.py
@@ -18,6 +18,7 @@ N = rospy.get_param('/num_of_robots',7)
 # Define the circle parameters
 circle_center = (50, 35)  # Center of the circle
 circle_radius = 20  # Radius of the circle
+sphero_radii = 3.6
 
 # Callback function to update robot positions
 def callback(*pos_datas):
@@ -60,41 +61,62 @@ def assign_goals():
     print('goals assigned')
 
 def move_robots(cmd_publishers):
-    # Implement logic to command the robots to move to their respective goals
-    # You should also implement collision avoidance here
+    # Implement logic to command the robots to move to their respective goals with collision avoidance
 
-    for id, loc in sphero_locs.items():
-        goal_loc = sphero_goals[id]
-
-        # Calculate the desired velocity to move towards the goal
-        desired_velocity = calculate_desired_velocity(loc, goal_loc)
+    for sphero_id, loc in sphero_locs.items():
+        goal_loc = sphero_goals[sphero_id]
+        avoid_locs = {loc_id: loc for loc_id, loc in sphero_locs.items() if loc_id != sphero_id}
+        # Calculate the desired velocity based on potential fields
+        desired_velocity = calculate_desired_velocity(loc, goal_loc, avoid_locs)
 
         # Publish the desired velocity to control the robot
         cmd_vel = Twist()
         cmd_vel.linear.x = desired_velocity[0]
         cmd_vel.linear.y = desired_velocity[1]
-        cmd_publishers[id].publish(cmd_vel)
+        cmd_publishers[sphero_id].publish(cmd_vel)
 
         # Check if the robot has reached its goal
-        if distance_to_goal(loc, goal_loc) < 2:  # You can adjust the threshold as needed
-            print(f"Sphero {id} has reached its goal!")
+        distance_to_goal = math.sqrt((loc[0] - goal_loc[0]) ** 2 + (loc[1] - goal_loc[1]) ** 2)
+        print(f"sphero_{sphero_id} distance to goal: {round(distance_to_goal,1)}")
 
-def calculate_desired_velocity(current_loc, goal_loc):
-    # Implement logic to calculate the desired velocity based on current and goal locations
-    # You can use PID control or other methods for smooth movement
+def calculate_desired_velocity(current_loc, goal_loc, avoid_locs):
+    # Implement logic to calculate the desired velocity based on potential fields
+    attractive_potential = calculate_attractive_potential(current_loc, goal_loc)
+    repulsive_potential = calculate_repulsive_potential(current_loc, avoid_locs)
+    
+    # Calculate the gradient of the total potential field
+    gradient_x = attractive_potential[0] + repulsive_potential[0]
+    gradient_y = attractive_potential[1] + repulsive_potential[1]
 
-    # Example: simple proportional control
-    kp = 0.01
-    desired_velocity_x = kp * (goal_loc[0] - current_loc[0])
-    desired_velocity_y = kp * (goal_loc[1] - current_loc[1])
+    # Return the desired velocity components
+    return (gradient_x, gradient_y)
 
-    return (desired_velocity_x, desired_velocity_y)
+def calculate_attractive_potential(current_loc, goal_loc):
+    # Calculate the attractive potential field
+    k_att = 0.005
+    gradient_x = k_att * (goal_loc[0] - current_loc[0])
+    gradient_y = k_att * (goal_loc[1] - current_loc[1])
+    return (gradient_x, gradient_y)
+
+def calculate_repulsive_potential(current_loc, avoid_locs):
+    # Calculate the repulsive potential field based on robot positions
+    k_rep = 2.5  # Repulsion gain
+    min_dist = 15
+    gradient_x = 0.0
+    gradient_y = 0.0
+
+    for robot_id, robot_loc in avoid_locs.items():
+        distance = math.sqrt((current_loc[0] - robot_loc[0]) ** 2 + (current_loc[1] - robot_loc[1]) ** 2)
+        if distance < min_dist:
+            angle = math.atan2(current_loc[1] - robot_loc[1], current_loc[0] - robot_loc[0])
+            gradient_x += k_rep * ((1.0 / distance) - (1.0 / min_dist)) * math.cos(angle)
+            gradient_y += k_rep * ((1.0 / distance) - (1.0 / min_dist)) * math.sin(angle)
+
+    return (gradient_x, gradient_y)
 
 # Function to calculate the distance to the goal
 def distance_to_goal(current_loc, goal_loc):
     return math.sqrt((current_loc[0] - goal_loc[0]) ** 2 + (current_loc[1] - goal_loc[1]) ** 2)
-
-    
 
 def main():
     rospy.init_node('circler',anonymous=False)
